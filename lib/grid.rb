a3572f274d2633bcf6d29fb6bc7c94c891230eed
VAnto <vntantonio@gmail.com>
2023-03-05 18:52:11 +0100

    grid.rb style refactoring

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,70 +1,59 @@
-class Grid
+# frozen_string_literal: true
 
+# Grid creation and win conditions
+class Grid
   def initialize
     @@grid_rows = [
-             [1, 2, 3],
-             [4, 5, 6],
-             [7, 8, 9]
-            ]
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
     @@positioned_symbols = 0
-  end    
+  end
 
   def display
-    puts @@grid_rows[0].join("|")
-    puts "-+-+-"
-    puts @@grid_rows[1].join("|")
-    puts "-+-+-"
-    puts @@grid_rows[2].join("|")
+    puts @@grid_rows[0].join('|')
+    puts '-+-+-'
+    puts @@grid_rows[1].join('|')
+    puts '-+-+-'
+    puts @@grid_rows[2].join('|')
   end
 
   def win_condition
-    
     return true if horizontal_win == true || vertical_win == true || diagonal_win == true
 
-    if @@positioned_symbols == 9
-      return "Draw"
-    end
-    
+    return 'Draw' if @@positioned_symbols == 9
   end
 
   private
 
   def horizontal_win
-    @@grid_rows.each_with_index do | row, row_index |
-      if @@grid_rows[row_index].uniq.count == 1
-        return true
-      end
+    @@grid_rows.each_with_index do |_row, row_index|
+      return true if @@grid_rows[row_index].uniq.count == 1
     end
   end
-  
+
   def vertical_win
-    for i in 0..2
+    [0, 1, 2].each do |i|
       vertical = []
-      @@grid_rows.each do | row |
+      @@grid_rows.each do |row|
         vertical.push(row[i])
       end
-      if vertical.uniq.count == 1
-        return true
-      end
+      return true if vertical.uniq.count == 1
     end
   end
 
   def diagonal_win
     diagonal = []
-    @@grid_rows.each_with_index do | row, row_index |
+    @@grid_rows.each_with_index do |row, row_index|
       diagonal.push(row[row_index])
     end
-    if diagonal.uniq.count == 1
-      return true
-    end
+    return true if diagonal.uniq.count == 1
 
     diagonal = []
-    @@grid_rows.reverse.each_with_index do | row, row_index |
+    @@grid_rows.reverse.each_with_index do |row, row_index|
       diagonal.push(row[row_index])
     end
-    
-    if diagonal.uniq.count == 1
-      return true
-    end
+    return true if diagonal.uniq.count == 1
   end
 end
